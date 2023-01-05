023d8257b61dd5e540a467c2dcafcb08dec8ac5a
Marco Hernandez <MarcoAHVega@gmail.com>
2023-01-05 10:55:57 -0800

    added react cnfig

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,24 @@
 import '../styles/styles.scss'
 import TestJsFeature from './modules/TestJsFeature'
 
+// React Related Code Goes Here
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function MyAmazingComponent() {
+  return (
+    <div>
+      <h1>This is My Amazing React Component</h1>
+      <p>React is great!</p>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<MyAmazingComponent />)
+
+// React code end here
+
 let testJsFeature = new TestJsFeature()
 
 if (module.hot) {
