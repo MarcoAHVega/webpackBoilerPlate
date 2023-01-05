0341853f4ec12542523214db6e6a27c3cabea5f2
Marco Hernandez <MarcoAHVega@gmail.com>
2023-01-04 22:09:59 -0800

    update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const fse = require('fs-extra')
 class RunAfterCompile {
   apply(compiler) {
     compiler.hooks.done.tap('Copy images', function () {
-      fse.copySync('./app/assets/images', './dist/assets/images')
+      fse.copySync('./app/assets/images', './docs/assets/images')
     })
   }
 }
@@ -71,7 +71,7 @@ if (currentTask == 'build') {
   config.output = {
     filename: '[name].[chunkhash].js',
     chunkFilename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   }
   config.mode = 'production'
   config.optimization = {
